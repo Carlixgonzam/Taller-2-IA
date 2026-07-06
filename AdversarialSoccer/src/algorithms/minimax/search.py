76bc58d55411c3bf0ff99c5ad8ec756c1e4ee217
Ivonne Mariana Cuncanchun Bernal <i.cuncanchunb@uniandes.edu.co>
2026-07-06 17:44:45 -0500

    segun yo sirve... pero falta el evaluation function

--- AdversarialSoccer/src/algorithms/minimax/search.py
+++ AdversarialSoccer/src/algorithms/minimax/search.py
@@ -45,6 +45,41 @@ def minimax_search(
         ### YOUR CODE HERE ###
         # --- SOLUTION START ---
 
-        # --- SOLUTION END ---
+        if on_expand is not None:
+            on_expand()
 
+        if is_cutoff(state, depth):
+            return None, None, evaluation_function(state)
+
+        colombia_actions = legal_actions(state, Team.COLOMBIA)
+        rival_actions = legal_actions(state, Team.RIVAL)
+
+        best_colombia_action = None
+        best_rival_action = None
+        best_value = float("-inf")
+
+        for colombia_action in colombia_actions:
+
+            worst_rival_action = None
+            worst_value = float("inf")
+
+            for rival_action in rival_actions:
+
+                successor = step(state, colombia_action, rival_action)
+
+                value = ply(successor, depth - 1)[2]
+
+                if value < worst_value:
+                    worst_value = value
+                    worst_rival_action = rival_action
+
+            if worst_value > best_value:
+                best_value = worst_value
+                best_colombia_action = colombia_action
+                best_rival_action = worst_rival_action
+
+        return best_colombia_action, best_rival_action, best_value
+        
+        # --- SOLUTION END ---
+    
     return finish_search_root(*ply(state, depth))
